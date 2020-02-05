9911d97542444857c89171e84ea4afcc2684ff56
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2020-02-05 09:36:38 -0500

    Changes app to a class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App App-header">
-    </div>
-  );
+
+// need to make a component with a list of notes
+// list needs to be searchable
+// each item in the list should show a text-area form
+// the text in the text box should be saved to the global state as an object
+// when they close out of a note, the note data should be saved into an object
+// when they click on the item, it should display the note
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App App-header">
+      </div>
+    );
+  }
 }
 
 export default App;
